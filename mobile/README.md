4a1c7dde584271e5aae8db1918c0b60a8e8b6fc0
jeprcks <baclayjeffrey@gmail.com>
2024-12-16 19:30:08 +0800

    Delete mobile directory

--- mobile/README.md
+++
@@ -1,3 +0,0 @@
-# flutterproject2
-
-A new Flutter project.
--- mobile/android/app/src/main/kotlin/com/example/flutterproject2/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.flutterproject2
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
